bf19767e86d361233a38e839150ae3cda84d554b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 17:52:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
