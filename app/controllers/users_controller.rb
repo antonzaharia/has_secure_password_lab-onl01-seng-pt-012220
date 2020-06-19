e5345f27354ce54ee5e29548e21cd456f65fbe34
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 17:57:24 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:user).require(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
