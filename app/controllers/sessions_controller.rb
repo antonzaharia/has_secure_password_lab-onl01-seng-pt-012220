97375438fb8394085da22ef296ab4103c6e7b21e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 18:01:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
+      redirect_to welcome_path
     else
       redirect_to login_path, alert: "Wrong Password"
     end
