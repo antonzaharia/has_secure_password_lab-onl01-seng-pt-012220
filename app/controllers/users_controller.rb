5c9756853acc8d5a05ecd9ce502a7bab82ea39aa
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 17:50:21 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
   end
 
-  private
+  private 
   def user_params
-    params.permit(:user).require(:name, :password)
+    params.permit(:user).require(:name, :password, :password_confirmation)
   end
 end
