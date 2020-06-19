846faefac1177a60bce5e30c882b05ecbe2f481a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 17:59:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
   end
 end
